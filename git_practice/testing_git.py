356bb36ea165ddf20f1ad856a121746949d769ed
Paweł Tomkiewicz <patom4@onet.pl>
2023-10-09 20:25:07 +0200

    save on branch4

--- git_practice/testing_git.py
+++ git_practice/testing_git.py
@@ -1,6 +1,10 @@
-def def_math(a: int, b:int, c:int) ->int:
+def def_math(a: int, b: int, c: int) -> int:
     return a + b + c
 
-def math2(a:int, b:int, c:int) ->int:
+
+def math2(a: int, b: int, c: int) -> int:
     return a + b * c
 
+
+def hello():
+    return 'hello'
